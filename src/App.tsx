613a7d680a0b9ef2c5232b61f768039c3ace18fb
man0405 <vanman160504@gmail.com>
2024-11-13 23:11:37 +0700

    remove all project , and change to js

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-
-function App() {
-	var connect = new WebSocket("ws://localhost:8080/socket");
-
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
-
-export default App;
