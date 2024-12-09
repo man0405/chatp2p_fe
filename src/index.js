a53144dc81602a83e0e7e11795589fef61f24894
man0405 <vanman160504@gmail.com>
2024-12-09 20:31:48 +0700

    update index.js: switch from createBrowserRouter to createHashRouter for routing

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Test from "./page/test";
 import LoginPage from "./page/Login";
 import RegisterPage from "./page/Register";
 import Call from "./page/Call";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/test",
-    element: <Test />,
-  },
-  {
-    path: "/auth/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/auth/register",
-    element: <RegisterPage />,
-  },
-  {
-    path: "/call",
-    element: <Call />,
-  },
+const router = createHashRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+	{
+		path: "/test",
+		element: <Test />,
+	},
+	{
+		path: "/auth/login",
+		element: <LoginPage />,
+	},
+	{
+		path: "/auth/register",
+		element: <RegisterPage />,
+	},
+	{
+		path: "/call",
+		element: <Call />,
+	},
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router} />);
